2feab902990ccaabe4a9769053b4ef9df81efd90
Sheep <76005806+sheepalt@users.noreply.github.com>
2022-06-22 15:47:10 +0200

    Create check.c

---
+++ check.c
@@ -0,0 +1,25 @@
+#include<stdio.h>
+
+int main() {
+    int num,i,n,k,num1;
+    printf("Enter any number:");
+    scanf("%d",&num);
+    n=num;
+    i=0;
+    printf("\nEnter the digit:");
+    scanf("%d",&num);
+    
+    while(n!=0)
+    {
+        k = n%10;
+        n=n/10;
+        if(k==num1)
+        {
+            i++;
+        }
+    }
+    printf("\nThe occurrence of %d is %d times",num1,i);
+    
+    return 0;
+    
+}
